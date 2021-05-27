42d482b6b1fe1941cdc118c50654d78f8b4632f1
James Hughes <jhughes1010@gmail.com>
2021-05-27 03:42:55 -0600

    LED on ESP32 used to monitor anemometer

--- weather_vane_eval.ino
+++ weather_vane_eval.ino
@@ -1,13 +1,17 @@
 #define WIND_DIR_PIN 35
+#define LED           2  //Diagnostics using built-in LED
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(14, INPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, 0);
   Serial.begin(115200);
 }
 
 void loop() {
   int windPosition;
+  int windPin;
   String windDirection = "N";
   int analogCompare[15] = {150, 300, 450, 600, 830, 1100, 1500, 1700, 2250, 2350, 2700, 3000, 3200, 3400, 3900};
   //String windDirText[15] = {"SSW", "S", "WSW", "3", "SW", "W", "6", "ESE", "SE", "NNW", "NW", "ENE", "E", "NNE", "NE"}; //BLYNK does not seem to allow text
@@ -23,6 +27,9 @@ void loop() {
       break;
     }
   }
+  windPin= digitalRead(14);
+  digitalWrite(LED,!windPin);
+  Serial.printf("Wind speed switch %i\n", windPin);
   Serial.printf("Wind direction: %i - %i - %s\n", vin, windPosition, windDirText[windPosition]);
   delay(250);
 }
